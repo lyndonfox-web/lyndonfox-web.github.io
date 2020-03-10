835b72af7b2b4208e9f9627b147b97ede1d8f555
lyndonfox.web@gmail.com <lyndonjohnfox@Lyndons-iMac.local>
2020-03-10 19:23:42 +1300

    New website added

--- js/countUp-jquery.js
+++ js/countUp-jquery.js
@@ -1,39 +1,39 @@
-(function($) {
- 
-    $.fn.countup = function(params) {
- 		// make sure dependency is present
+(function ($) {
+
+    $.fn.countup = function (params) {
+        // make sure dependency is present
         if (typeof CountUp !== 'function') {
-        	console.error('countUp.js is a required dependency of countUp-jquery.js.');
-        	return;
+            console.error('countUp.js is a required dependency of countUp-jquery.js.');
+            return;
         }
 
         var defaults = {
-        	startVal: 0,
-        	decimals: 0,
-        	duration: 2,
+            startVal: 0,
+            decimals: 0,
+            duration: 2,
         };
 
         if (typeof params === 'number') {
-        	defaults.endVal = params;
+            defaults.endVal = params;
         }
         else if (typeof params === 'object') {
-        	$.extend(defaults, params);
+            $.extend(defaults, params);
         }
         else {
-        	console.error('countUp-jquery requires its argument to be either an object or number');
-        	return;
-        } 
+            console.error('countUp-jquery requires its argument to be either an object or number');
+            return;
+        }
 
-        this.each(function(i, elem) {
-        	var countUp = new CountUp(elem, defaults.startVal, defaults.endVal, defaults.decimals, defaults.duration, defaults.options);
+        this.each(function (i, elem) {
+            var countUp = new CountUp(elem, defaults.startVal, defaults.endVal, defaults.decimals, defaults.duration, defaults.options);
 
-        	countUp.start();
+            countUp.start();
         });
 
 
 
         return this;
- 
+
     };
- 
+
 }(jQuery));
